0ca4de557b2e5097e002bf60533c07e845c00b2a
Maciej Mazur <mr.mazurmaciej@gmail.com>
2019-03-31 17:08:29 +0200

    I added setState to out project in App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ class App extends Component {
   
   state = {
     persons: [
-      { name:'Macieja', age:29 },
-      { name: 'Olimpia', age:29 },
+      { name:'Maciej', age:29 },
+      { name: 'Olimpiada', age:29 },
       { name: 'Jakubek', age:39 },
       { name: 'Miłoszo', age:21 },
       { name: 'Mikołajo', age:32 }
+    ],
+    otherState: "some new info"
+  }
+
+  switchButtonHandler = () => {
+    this.setState({
+      persons: [
+        { name:'Maciej', age:29 },
+        { name: 'Olimpiada', age:29 },
+        { name: 'Jakubeka', age:39 },
+        { name: 'Miłoszo', age:21 },
+        { name: 'Mikołajow', age:32 }
     ]
+    })
   }
   
   render() {
@@ -23,7 +36,7 @@ class App extends Component {
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> I really like travel!</Person>
-        <button>Switch Name!</button>
+        <button onClick={this.switchButtonHandler}>Switch Name!</button>
         <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
         <Person name={this.state.persons[4].name} age={this.state.persons[4].age}/>
       </div>
