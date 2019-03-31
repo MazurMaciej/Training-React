a6c8e1cbeb89ba78f85a8d7f22d00083fe53bc39
Maciej Mazur <mr.mazurmaciej@gmail.com>
2019-03-31 17:45:39 +0200

    I changed my component on funcion component with hoocs

--- src/App.js
+++ src/App.js
@@ -1,52 +1,59 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Person from './Person/Person'
+import Person from "./Person/Person";
 
-class App extends Component {
-  
-  state = {
+const App = () => {
+  const [personsState, setPersonsState] = useState({
     persons: [
-      { name:'Maciej', age:29 },
-      { name: 'Olimpiada', age:29 },
-      { name: 'Jakubek', age:39 },
-      { name: 'Miłoszo', age:21 },
-      { name: 'Mikołajo', age:32 }
+      { name: "Maciej", age: 29 },
+      { name: "Olimpiada", age: 29 },
+      { name: "Jakubek", age: 39 },
+      { name: "Miłoszo", age: 21 },
+      { name: "Mikołajo", age: 32 }
     ],
     otherState: "some new info"
-  }
+  });
 
-  switchButtonHandler = () => {
-    this.setState({
+  const switchButtonHandler = () => {
+    setPersonsState({
       persons: [
-        { name:'Maciej', age:29 },
-        { name: 'Olimpiada', age:29 },
-        { name: 'Jakubeka', age:39 },
-        { name: 'Miłoszo', age:21 },
-        { name: 'Mikołajow', age:32 }
-    ]
-    })
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <h1>
-          Hi, i'm a React App
-        </h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> I really like travel!</Person>
-        <button onClick={this.switchButtonHandler}>Switch Name!</button>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
-        <Person name={this.state.persons[4].name} age={this.state.persons[4].age}/>
-      </div>
-    );
-
-    // return (
-    //   React.createElement('div', {className: 'App'},React.createElement('h1', null, 'im very busy now' ))
-    // )
-
-  }
-}
+        { name: "Maciej", age: 29 },
+        { name: "Olimpiada", age: 29 },
+        { name: "Jakubeka", age: 39 },
+        { name: "Miłoszo", age: 21 },
+        { name: "Mikołajow", age: 32 }
+      ]
+    });
+  };
+  return (
+    <div className="App">
+      <h1>Hi, i'm a React App</h1>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      />
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      >
+        {" "}
+        I really like travel!
+      </Person>
+      <button onClick={switchButtonHandler}>Switch Name!</button>
+      <Person
+        name={personsState.persons[3].name}
+        age={personsState.persons[3].age}
+      />
+      <Person
+        name={personsState.persons[4].name}
+        age={personsState.persons[4].age}
+      />
+    </div>
+  );
+};
 
 export default App;
