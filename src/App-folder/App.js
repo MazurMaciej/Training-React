8d5e07f19e087aea58f37e6f0919f9dae932c27f
Maciej Mazur <mr.mazurmaciej@gmail.com>
2019-04-01 17:38:57 +0200

    Second metod to swtich f.e.menu. We add let with null to render but before return, and we add the condition + add to return in place where we want render <div> in jsx f.e {persons}

--- src/App-folder/App.js
+++ src/App-folder/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       { name: "Mikołajo", age: 32 }
     ],
     otherState: "some new info",
-    showPersons: false,
+    showPersons: false
   };
 
   switchButtonHandler = newName => {
@@ -41,9 +41,8 @@ class App extends Component {
 
   toggleOnList = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
+    this.setState({ showPersons: !doesShow });
   };
-
 
   render() {
     const styleButton = {
@@ -54,42 +53,46 @@ class App extends Component {
       cursor: "pointer"
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons =(
+        <div>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          changed={this.nameChangeHandler}
+        >
+          {" "}
+          I really like travel!
+        </Person>
+        <Person
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
+        />
+        <Person
+          name={this.state.persons[4].name}
+          age={this.state.persons[4].age}
+        />
+      </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, i'm a React App</h1>
-        <button
-          style={styleButton}
-          onClick={this.toggleOnList}>
+        <button style={styleButton} onClick={this.toggleOnList}>
           Switch Name!
         </button>
-        {
-        this.state.showPersons ?
-        <div >
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-          />
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            changed={this.nameChangeHandler}
-          >
-            {" "}
-            I really like travel!
-          </Person>
-          <Person
-            name={this.state.persons[3].name}
-            age={this.state.persons[3].age}
-          />
-          <Person
-            name={this.state.persons[4].name}
-            age={this.state.persons[4].age}
-          />
-        </div> : null}
+        {persons}
       </div>
     );
 
