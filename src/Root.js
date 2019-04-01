2e4d407c907e2b02c7572cd3654a29c7a9929c5c
Maciej Mazur <mr.mazurmaciej@gmail.com>
2019-04-01 13:13:15 +0200

    I changed strukture of files and folders. I created new folder App-folder with all project files with 31.03.2019 training. Next, i create new file Root.js, it will be new file to training excercise

---
+++ src/Root.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Root extends Component {
+
+    render () {
+
+        return(
+            <h1>Hello Maciej</h1>
+
+
+
+
+        )
+    }
+}
+export default Root;
